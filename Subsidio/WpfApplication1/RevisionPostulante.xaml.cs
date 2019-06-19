ffdd135ae013296b6323f74603e527daa97e0730
hidalgog <hidalgog@users.noreply.github.com>
2019-06-19 17:05:15 -0400

    Arreglo llamada funciones

--- Subsidio/WpfApplication1/RevisionPostulante.xaml.cs
+++ Subsidio/WpfApplication1/RevisionPostulante.xaml.cs
@@ -53,58 +53,90 @@ namespace WpfApplication1
             }
         }
 
-        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        private async void btnCalcular_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                //FN PUNTAJE CARGAS FAMILIARES
-                listaPostulantes list = new listaPostulantes(); //PARA LLAMAR DATOS ?)
+
+
                 Postulante pos = new Postulante();
 
+                //FN PUNTAJE CARGA----------------------------------------------------------
                 OracleCommand cmd = new OracleCommand("FN_PUNTAJE_CARG_FAM", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("rut", OracleDbType.Varchar2).Value = txtRut.Text;
+                //parametro de salida (out)
+                //La variable v_punt guarda lo que retorna la función
+                OracleParameter v_punt = cmd.Parameters.Add("v_punt", OracleDbType.Int32);
+                v_punt.Direction = ParameterDirection.ReturnValue;
 
-                //NECESARIO RESCATAR VARIABLE SALIDA?
-                /*OracleParameter output = cmd.Parameters.Add("v_punt", OracleDbType.Int32);
-                output.Direction = ParameterDirection.ReturnValue;
-                string cargas = output.ToString();*/
+                //parametros de entrada (in)
+                //la variable RUT es de entrada 
+                OracleParameter rut = cmd.Parameters.Add("rut", OracleDbType.Varchar2);
+                rut.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                rut.Value = txtRut.Text;//se le carga un valor desde caja texto
 
-                string cargas = cmd.ExecuteNonQuery().ToString();
+                cmd.Parameters.Add(rut);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtCargas.Text = cmd.Parameters["v_punt"].Value.ToString();//Se ejecuta y se muestra
 
-                txtCargas.Text = cargas;
 
-                //FN PUNTAJE ESTADO 
+
+                //FN PUNTAJE ESTADO------------------------------------------------------- 
                 cmd = new OracleCommand("FN_PUNTAJE_CIVIL", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("rut", OracleDbType.Varchar2).Value = txtRut.Text;
+                //Parametro de salida (out)
+                OracleParameter v_puntaje = cmd.Parameters.Add("v_puntaje", OracleDbType.Int32);
+                v_puntaje.Direction = ParameterDirection.ReturnValue;
 
-                string civil = cmd.ExecuteNonQuery().ToString();
+                //Parametros de entrada (in)
+                OracleParameter v_rut = cmd.Parameters.Add("rut", OracleDbType.Varchar2);
+                v_rut.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                v_rut.Value = txtRut.Text;//se le carga un valor desde caja texto
 
-                txtEstadoCivil.Text = civil;
+
+                cmd.Parameters.Add(v_rut);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtEstadoCivil.Text = cmd.Parameters["v_puntaje"].Value.ToString();//Se ejecuta y se muestra
 
 
-                //FN PUNTAJE AHORRO, COMO LLAMO AL MONTO DE ESE POSTULANTE?
+
+                //FN PUNTAJE AHORRO, COMO LLAMO AL MONTO DE ESE POSTULANTE?------------------------
                 cmd = new OracleCommand("FN_AHORRO", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("monto", OracleDbType.Int32).Value = list.Monto_Ahorro;
+                //Parametro de salida (out)
+                OracleParameter punt = cmd.Parameters.Add("v_puntaje", OracleDbType.Int32);
+                punt.Direction = ParameterDirection.ReturnValue;
 
-                string monto = cmd.ExecuteNonQuery().ToString();
+                //Parametros de entrada (in)
+                OracleParameter monto = cmd.Parameters.Add("monto", OracleDbType.Int32);
+                monto.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                monto.Value = pos.MONTO_AHORRO;//se le carga un valor desde caja texto
 
-                txtAhorro.Text = monto;
 
-                //FN PUNTAJE PUEBLO
+                cmd.Parameters.Add(monto);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtAhorro.Text = cmd.Parameters["v_puntaje"].Value.ToString();//Se ejecuta y se muestra
+
+                //FN PUNTAJE PUEBLO---------------------------------------------------
                 cmd = new OracleCommand("FN_PUEBLO", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("pueblo", OracleDbType.Varchar2).Value = list.Pueblo;
+                //salida
+                OracleParameter puntajes = cmd.Parameters.Add("v_puntaje", OracleDbType.Int32);
+                puntajes.Direction = ParameterDirection.ReturnValue;
 
-                string pueblo = cmd.ExecuteNonQuery().ToString();
+                //Parametros de entrada (in)
+                OracleParameter ruts = cmd.Parameters.Add("rut", OracleDbType.Varchar2);
+                ruts.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                ruts.Value = txtRut.Text;//se le carga un valor desde caja texto
 
-                txtPueblo.Text = pueblo;
+
+                cmd.Parameters.Add(ruts);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtPueblo.Text = cmd.Parameters["v_puntaje"].Value.ToString();//Se ejecuta y se muestra
 
                 //FN PUNTAJE TITULO
                 cmd = new OracleCommand("FN_TITULO", conn);
@@ -112,49 +144,102 @@ namespace WpfApplication1
 
                 cmd.Parameters.Add("id_tit", OracleDbType.Int32).Value = pos.ID_TITULO;
 
-                string titulo = cmd.ExecuteNonQuery().ToString();
+                // salida
+                OracleParameter valor = cmd.Parameters.Add("v_puntaje", OracleDbType.Int32);
+                valor.Direction = ParameterDirection.ReturnValue;
 
-                txtTitulo.Text = titulo;
+                //Parametros de entrada (in)
+                OracleParameter titulo = cmd.Parameters.Add("id_tit", OracleDbType.Int32);
+                titulo.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                titulo.Value = pos.ID_TITULO;//se le carga un valor desde caja texto
+
+
+                cmd.Parameters.Add(titulo);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtTitulo.Text = cmd.Parameters["v_puntaje"].Value.ToString();//Se ejecuta y se muestra
 
                 //FN PUNTAJE EDAD
                 cmd = new OracleCommand("FN_EDAD", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("fecha", OracleDbType.Date).Value = list.Fecha_Nacimiento;
+                //salida
+                OracleParameter p = cmd.Parameters.Add("v_puntaje", OracleDbType.Int32);
+                p.Direction = ParameterDirection.ReturnValue;
 
-                string edad = cmd.ExecuteNonQuery().ToString();
+                //Parametros de entrada (in)
+                OracleParameter fecha = cmd.Parameters.Add("fecha", OracleDbType.Date);
+                fecha.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                fecha.Value = pos.FECHA_NACIMIENTO;//se le carga un valor desde caja texto
 
-                txtEdad.Text = edad;
+                cmd.Parameters.Add(fecha);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtEdad.Text = cmd.Parameters["v_puntaje"].Value.ToString();//Se ejecuta y se muestra
+
+
+
 
                 //FN PUNTAJE ZONA EXTREMA--
                 cmd = new OracleCommand("FN_PORC_REGION", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("id_region", OracleDbType.Int32).Value = pos.ID_REGION;
+                //salida
+                OracleParameter porcentaje = cmd.Parameters.Add("v_porcentaje", OracleDbType.Int32);
+                porcentaje.Direction = ParameterDirection.ReturnValue;
 
-                string region = cmd.ExecuteNonQuery().ToString();
+                //Parametros de entrada (in)
+                OracleParameter region = cmd.Parameters.Add("id_region", OracleDbType.Date);
+                region.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                region.Value = pos.ID_REGION;//se le carga un valor desde caja texto
 
-                txtRegion.Text = region;
+
+                cmd.Parameters.Add(region);//agrego el parámetro de entrada al comando
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtEdad.Text = cmd.Parameters["v_porcentaje"].Value.ToString();//Se ejecuta y se muestra
 
                 //TOTAL
                 cmd = new OracleCommand("FN_TOTAL", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.Add("fecha", OracleDbType.Date).Value = list.Fecha_Nacimiento;
-                cmd.Parameters.Add("monto", OracleDbType.Int32).Value = list.Monto_Ahorro;
-                cmd.Parameters.Add("pueblo", OracleDbType.Varchar2).Value = list.Pueblo;
-                cmd.Parameters.Add("id_tit", OracleDbType.Int32).Value = pos.ID_TITULO;
-                cmd.Parameters.Add("id_region", OracleDbType.Int32).Value = pos.ID_REGION;
+                //SALIDA
+                OracleParameter p_total = cmd.Parameters.Add("v_puntaje", OracleDbType.Int32);
+                p_total.Direction = ParameterDirection.ReturnValue;
 
-                string total = cmd.ExecuteNonQuery().ToString();
+                //ENTRADA------------------
+                //FECHA
+                OracleParameter fec = cmd.Parameters.Add("fecha", OracleDbType.Date);
+                fec.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                fec.Value = pos.FECHA_NACIMIENTO;
 
-                txtCalculo.Text = total;
+                //MONTO
+                OracleParameter mont = cmd.Parameters.Add("monto", OracleDbType.Int32);
+                mont.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                mont.Value = pos.MONTO_AHORRO;//se le carga un valor desde caja texto
+
+                //TITULO
+                OracleParameter tit = cmd.Parameters.Add("id_tit", OracleDbType.Int32);
+                tit.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                tit.Value = pos.ID_TITULO;//se le carga un valor desde caja texto
+
+                //REGION
+                OracleParameter reg = cmd.Parameters.Add("id_region", OracleDbType.Date);
+                reg.Direction = ParameterDirection.Input;//contiene el parametro de entrada
+                reg.Value = pos.ID_REGION;//se le carga un valor desde caja texto
+
+
+                cmd.Parameters.Add(fec);//agrego el parámetro de entrada al comand
+                cmd.Parameters.Add(mont);
+                cmd.Parameters.Add(tit);
+                cmd.Parameters.Add(reg);
+                cmd.ExecuteNonQuery();//se ejecuta la función
+                txtCalculo.Text = cmd.Parameters["v_puntaje"].Value.ToString();//Se ejecuta y se muestra
+
 
 
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error al Cargar datos");
+                await this.ShowMessageAsync("Mensaje:",
+                         string.Format("Error al cargar datos"));
                 Logger.Mensaje(ex.Message);
             }
             //conn.Close();
